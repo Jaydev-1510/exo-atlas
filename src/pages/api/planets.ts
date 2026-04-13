f74b6f03d54df79d98423cea6f0ffb938eae049c
Jaydev Parmar <jydev@duck.com>
2026-04-13 17:13:10 +0530

    Add SSR to API

--- src/pages/api/planets.ts
+++ src/pages/api/planets.ts
@@ -1,6 +1,8 @@
 import type { APIRoute } from "astro";
 import { supabase } from "@/lib/supabase";
 
+export const prerender = false;
+
 export const GET: APIRoute = async ({ url }) => {
   const params = url.searchParams;
 
