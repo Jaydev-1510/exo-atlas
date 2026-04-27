54a98e4eecfa1bce11c1dfb09250d2c7b989c34b
Jaydev Parmar <jydev@duck.com>
2026-04-27 16:25:12 +0530

    Add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel";
 import path from "path";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
@@ -309,4 +310,5 @@ export default defineConfig({
       exclude: [/^\/api\/.+/],
     },
   }),
+  integrations: [react()],
 });
